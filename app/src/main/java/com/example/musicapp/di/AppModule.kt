5d26ded56f1e9ef608db180fddbe1c3a36fc3ae5
Kaluga <karol.kaluga@capgemini.com>
2022-04-20 09:47:17 +0200

    Changes in TrackDatabase.kt. Create AppModule.kt in di package. Create fun provideTrackDatabase() in AppModule.kt

--- app/src/main/java/com/example/musicapp/di/AppModule.kt
+++ app/src/main/java/com/example/musicapp/di/AppModule.kt
@@ -1,4 +1,22 @@
 package com.example.musicapp.di
 
+import android.app.Application
+import androidx.room.Room
+import com.example.musicapp.database.TrackDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    fun provideTrackDatabase(app: Application): TrackDatabase{
+        return Room.databaseBuilder(
+            app,
+            TrackDatabase::class.java,
+            TrackDatabase.DATABASE_NAME).build()
+    }
 }
